b07909d1651e430ca2f76e29b2a47c44c68c3f4d
Krish12003 <39015956+Krish12003@users.noreply.github.com>
2020-03-19 18:36:13 -0400

    Add a register command. This registers the user on the database

--- Cogs/LawBot.py
+++ Cogs/LawBot.py
@@ -14,5 +14,13 @@ class LawBot(commands.Cog):
     async def hi(self,ctx):
         await ctx.send("Hi")
 
+    @commands.command()
+    async def register(self,ctx):
+    	"""Opens up a bank account for the user."""
+    	if len(data.search(User.id == ctx.author.id)) != 0:
+    		return await ctx.send("You already have a bank account registered.")
+    	data.insert({"id":ctx.author.id,"cash":500,"balance":0,"lastJobTime":None,"lastCrimeTime":None})
+    	await ctx.send(f"<@{ctx.author.id}> registered successfully! Your starting balance is **¥500**")
+
 def setup(bot):
     bot.add_cog(LawBot(bot))     
